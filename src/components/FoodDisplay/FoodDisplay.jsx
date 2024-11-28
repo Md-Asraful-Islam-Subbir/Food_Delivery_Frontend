97f1aa6c25aac482212aeef6414e79ad34bd6e7b
Md-Asraful-Islam-Subbir <asraful17@cse.pstu.ac.bd>
2024-11-28 10:54:43 +0600

    fa

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -14,9 +14,13 @@ const FoodDisplay = ({ category }) => {
           <p>No items available</p>
         ) : (
           food_list.map((item, index) => {
-            if (category === "All" || category === item.category) {
-              return (
-                <div key={index} className={`food-item ${item.inStock ? "" : "out-of-stock"}`}>
+            const isCategoryMatch = category === "All" || category === item.category;
+            return (
+              isCategoryMatch && (
+                <div
+                  key={index}
+                  className={`food-item ${item.inStock ? "" : "out-of-stock"}`}
+                >
                   <FoodItem
                     id={item._id}
                     name={item.name}
@@ -26,9 +30,8 @@ const FoodDisplay = ({ category }) => {
                   />
                   {!item.inStock && <p className="stock-status">Out of Stock</p>}
                 </div>
-              );
-            }
-            return null;
+              )
+            );
           })
         )}
       </div>
