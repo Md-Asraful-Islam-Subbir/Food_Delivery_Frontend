2c22258a030b3be3d25be3eecdd8d2435b402efe
Md-Asraful-Islam-Subbir <93644909+Md-Asraful-Islam-Subbir@users.noreply.github.com>
2026-03-05 13:17:32 +0600

    logout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,56 +1,58 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
-import {assets} from '../../assets/assets'
+import { assets } from '../../assets/assets'
 import { Link, useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { StoreContext } from '../../context/StoreContext';
 
 
-const Navbar = ({setShowLogin}) => {
-    const [menu,setMenu]=useState("home");
-    const {getTotalCartAmount,token,setToken}=useContext(StoreContext);
-  
-    const navigate=useNavigate();
+const Navbar = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState("home");
+  const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
 
-    const logout=()=>{
-      localStorage.removeItem("token");
-      setToken("");
-      navigate("/");
-    }
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    navigate("/");
+  }
 
 
-    return (
+  return (
     <div className='navbar'>
-   <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
+      <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
 
-    <ul className="navbar-menu">
-        <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
-        <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
-        <a href='/aboutus' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>About Us</a>
-        <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
-    </ul>
+      <ul className="navbar-menu">
+        <Link to='/' onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</Link>
+        <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
+        <a href='/aboutus' onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>About Us</a>
+        <a href='#footer' onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>contact us</a>
+      </ul>
 
-    <div className="navbar-right">
-        <img src={assets.search_icon} alt="" />
-        <div className="navbar-search-icon">
-          <Link to='/cart'>  <img src={assets.basket_icon} alt="" /></Link>
-            <div className={getTotalCartAmount()===0?"":"dot"}>
-
-            </div>
+      <div className="navbar-right">
+         <SearchBar />
+            <div className='navbar-search-icon'>
+               <Link to="/cart"> <img src={assets.basket_icon} alt="" /></Link>
+               {cartQty > 0 && (
+                  <div className="cart-count">
+                     {cartQty}
+                  </div>
+               )}
         </div>
-        
-        {!token?<button onClick={()=>setShowLogin(true)}>sign in</button>
-        :<div className='navbar-profile'>
-<img src={assets.profile_icon} alt="" />
-<ul className="navbar-profile-dropdown">
-  <li onClick={()=>navigate('/myorders')}><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
-  <hr/>
-  <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
-</ul>
+
+        {!token ? <button onClick={() => setShowLogin(true)}>sign in</button>
+          : <div className='navbar-profile'>
+            <img src={assets.profile_icon} alt="" />
+            <ul className="navbar-profile-dropdown">
+              <li onClick={() => navigate('/myorders')}><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+              <hr />
+              <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+            </ul>
           </div>
-          }
-        
-    </div>
+        }
+
+      </div>
     </div>
   )
 }
