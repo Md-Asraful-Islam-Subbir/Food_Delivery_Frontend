54df6b16a22fef2a9994605cd92b10a8aaefb5b6
Md-Asraful-Islam-Subbir <93644909+Md-Asraful-Islam-Subbir@users.noreply.github.com>
2026-03-05 12:46:55 +0600

    logout

--- src/App.jsx
+++ src/App.jsx
@@ -8,34 +8,40 @@ import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 import Verify from './pages/Verify/Verify'
 import MyOrders from './pages/MyOrders/MyOrders'
-import AboutPage from './Pages/About/AboutPage'
+import AboutPage from './pages/About/AboutPage'
+import { ToastContainer, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import SearchResult from './Pages/SearchResult/SearchResult'
+import Cursor from './GSAP/Cursor'
 
 const App = () => {
-  const [showLogin,setShowLogin]=useState(false)
+  const [showLogin, setShowLogin] = useState(false)
   return (
     <>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
-    <div className='app'>
-<Navbar setShowLogin={setShowLogin}/>
-<Routes>
-  <Route path='/' element={<Home/>} />
-  <Route path='/cart' element={<Cart/>} />
-  <Route path='/order' element={<PlaceOrder/>} />
-  <Route path='/verify' element={<Verify/>} />
-  <Route path='/myorders' element={<MyOrders/>} />
-  <Route path='/aboutus' element={<AboutPage/>}/>
-</Routes>
-    </div>
-    <Footer/>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
+      <div className='app'>
+        <Cursor />
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+           <Route path='/search' element={<SearchResult />} />
+          <Route path='/verify' element={<Verify />} />
+          <Route path='/myorders' element={<MyOrders />} />
+          <Route path='/aboutus' element={<AboutPage />} />
+        </Routes>
+      </div>
+      <Footer />
       <ToastContainer
-        position="top-right" autoClose={2000} hideProgressBar={false}  newestOnTop={false}  closeOnClick={false}  rtl={false}    pauseOnFocusLoss
+        position="top-right" autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick={false} rtl={false} pauseOnFocusLoss
         draggable
         pauseOnHover
         theme="light"
         transition={Bounce}
       />
     </>
-    
+
   )
 }
 
