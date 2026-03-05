f2b0ce231c04c175481bc1ebfa3f28b407b79745
Md-Asraful-Islam-Subbir <93644909+Md-Asraful-Islam-Subbir@users.noreply.github.com>
2026-03-05 15:37:15 +0600

    logout

--- src/components/Navbar/SearchBar.jsx
+++ src/components/Navbar/SearchBar.jsx
@@ -5,40 +5,16 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
 
 export default function SearchBar() {
-
-    const { searchTerm, setSearchTerm} = useContext(StoreContext);
-    const [inputValue, setInputValue] = useState('')
+    const { setSearchTerm } = useContext(StoreContext);
+    const [inputValue, setInputValue] = useState('');
     const navigate = useNavigate();
-    const location = useLocation();
-
-    useEffect(() => {
-        if (location.pathname !== '/search') {
-            setInputValue('')
-        }
-    }, [location.pathname])
 
     const handleKey = (e) => {
         if (e.key === "Enter" && inputValue.trim() !== "") {
-            setSearchTerm(inputValue)
-            localStorage.setItem('searchTerm', inputValue);
-            navigate('/search')
+            setSearchTerm(inputValue);
+            navigate('/search');
         }
     }
-
-    useEffect(() => {
-        if (
-            location.pathname === "/search" &&
-            inputValue.trim() === ""
-        ) {
-            const timer = setTimeout(() => {
-                setSearchTerm("");
-                navigate("/");
-            }, 2000);
-
-            return () => clearTimeout(timer);
-        }
-    }, [inputValue, location.pathname, navigate, setSearchTerm]);
-
 
     return (
         <div className="navbar-search">
